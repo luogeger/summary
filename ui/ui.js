b5169098a0e402a3b27ce726b9ba237f539a1f21
luogeger <xiaoqing1232@163.com>
2017-08-15 18:19:45 +0800

    model - update

--- ui/ui.js
+++ ui/ui.js
@@ -325,18 +325,17 @@
 
     function uiModel (ele){
         $(ele).wrap(("<div id='mask-layer'></div>"));// 给 id 元素包裹 遮罩层
-        $(ele).animate({'top': '0px'}, 300);// 弹窗内容 显示
-        $(ele).click(function (e){  e.stopPropagation(); });// 阻止冒泡
         $('body').css({'overflow': 'hidden'});// body 滚动条消失
-        $('#mask-layer').css({'overflow-y': 'scroll'});// 背景层的滚动条显示
+        $(ele).animate({'top': '0px'}, 300, function (){
+            $('#mask-layer').css({'overflow-y': 'auto'});// 背景层的滚动条显示
+        });// 弹窗内容 显示
+        $(ele).click(function (e){  e.stopPropagation(); });// 阻止冒泡
 
         $('#mask-layer').click(function (){
-            $('body').css({'overflow': 'auto'});
-            $('#mask-layer').css({'overflow-y': 'auto'});
-
             $(ele).animate({'top': '-5000px'}, 300, function (){
                 $(this).parent().before($(this));
                 $('#mask-layer').remove();
+                $('body').css({'overflow': 'auto'});
             });
         });
 
