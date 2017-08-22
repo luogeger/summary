bfc65823f8878534d6e80e086d401f52ccd7bd0c
luogeger <xiaoqing1232@163.com>
2017-08-22 11:20:40 +0800

    accordion update

--- ui/ui.js
+++ ui/ui.js
@@ -278,19 +278,20 @@
             // hover -- 效果
             _item.children('a').hover(function (){
                 var _this = $(this);
+                if(_this.hasClass('flag-open')){
+                    return;
+                }
                 _this.toggleClass('a-hover');
             },function (){
                 var _this = $(this);
-                _this.toggleClass('a-hover');
+                _this.removeClass('a-hover');
             })
 
             // 点击 -- 展开目录
             if(_item.has('ul').length){
-                // 如果有 子级菜单
-                collapse(_item, 'if');// 展开目录
+                collapse(_item, 'if');// 展开目录 -- 如果有 子级菜单
             }else{
-                // 没有 子级菜单
-                collapse(_item);// 展开目录
+                collapse(_item);// 展开目录 -- 没有 子级菜单
             };
         });
 
@@ -301,26 +302,33 @@
                 _item.children('ul').css({'display': 'none'});
                 _item.children('a').click(function (){
                     var _this = $(this);
+                    clearClick();// 遍历所有 a 标签，清除 点击样式
                     if(_this.hasClass('flag-open')){
                         _this.siblings('ul').slideToggle(100);
                         _this.children('i').css({'transform':'rotate(0deg)'});
                         _this.removeClass('flag-open');
+                        _this.addClass('a-hasChild');
                         return;
                     }
                     _this.siblings('ul').slideToggle(100);
                     _this.children('i').css({'transform':'rotate(90deg)'});
-                    _this.addClass('flag-open');
+                    _this.addClass('flag-open a-hasChild');
                 });
             }
             else{
                 _item.children('a').prepend('<i class="icon-circle"></i>');
                 _item.children('a').click(function (){
-                    $('#accordion li').each(function (i, v){
-                        $(v).children('a').removeClass('a-click-no');
-                    })
-                    $(this).addClass('a-click-no');
+                    clearClick();// 遍历所有 a 标签，清除 点击样式
+                    $(this).addClass('a-noChild');
                 });
-            }
+            };
+
+            // 遍历所有 a 标签，清除 点击样式
+            function clearClick (){
+                $('#accordion li').each(function (i, v){
+                    $(v).children('a').removeClass('a-noChild a-hasChild');
+                })
+            };
         };
     })();
 
