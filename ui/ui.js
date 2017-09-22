99ab8539dd9e12cc77cafa8a002639bae86acf76
luogeger <xiaoqing1232@163.com>
2017-09-22 17:35:52 +0800

    page-table

--- ui/ui.js
+++ ui/ui.js
@@ -7,7 +7,7 @@ console.log('ui');
  *   动态获取 data-value
  *   1. 如果下拉框里面的数据如果是动态获取的，就要在执行一次这个函数
  * */
-window.dynamicDataValue = '';
+// window.dynamicDataValue = '';
 
 /*
  *   点击 document 的事件
@@ -27,132 +27,139 @@ $(document).click(function (){
     $('.datePicker .dateBox').slideUp(10);
 });
 
-//  -----------------------------------------------------------------------
 
-
-
-/*
- *   下拉框 -- 自定义属性data-value
- *   1. 下拉框的的宽度保持一致
- * */
-// 1. 下拉框的的宽度保持一致
-$('.dropdown').children('ul').each(function (index, item){
-    var width = $(item).parent('.dropdown').css('width');
-    $(item).css('width', width);
-})
-
-$('.dropdown').each(function (index, item){
-    //追加icon
-    $(item).prepend('<i class="icon-arrow-down"></i>');
-
-    // a 标签设置 自定义属性
-    $(item).children('a').attr('data-value', '');
-
-    // li 标签设置 自定义属性
-    $(item).children('ul').each(function (index, item){
-        $(item).children().each(function (index, item){
-            $(item).attr('data-value', index);
-        })
-    });
-
-    // 点击输入框
-    $(item).children('a').click(function (e){
-        e.stopPropagation();
-        if($(this).parent().hasClass('flag-open')){
-            $(this).siblings('i').css({'transform':'rotate(0deg)'});
-            $(this).siblings('ul').slideUp(100);
-            $(this).parent().removeClass('flag-open').toggleClass('drop-hover-border');
-            return;
+inputInit();
+function inputInit (){
+    /*
+     *   输入框
+     *   1. readonly 属性
+     *   2. 是否添加 tip 提示
+     * */
+    $('.input-group-xs, .input-group-sm, .input-group-md, .input-group-lg').each(function (index, item){
+        if($(item).hasClass('readonly')){
+            $(this).children('input').attr('readonly', 'readonly');
         }
-        $('.dropdown').removeClass('flag-open').removeClass('drop-hover-border');
-        $('.dropdown ul').css({'display':'none'});// 点击页面任何地方，下拉框隐藏
-        $('.dropdown i').css({'transform':'rotate(0deg)'});
-        $(this).parent().addClass('flag-open').toggleClass('drop-hover-border');
-        $(this).siblings('i').css({'transform':'rotate(180deg)'});
-        $(this).siblings('ul').slideToggle(100);
+
+        if($(item).hasClass('tip-verify')){
+            $(this).append('<span class="tip">已经超过6个字</span>');
+        }
+    })
+
+
+    /*
+     *   单选框
+     *   1. 只有最后一个包含 checked 的 input 才能被选中
+     *   2. 点击事件
+     * */
+    var items = []; // 所有包含 checked 属性的 input-radio
+    $('.radio-group input:radio').each(function (index, item){
+        var spans = '<span class="icon-circle-empty"></span><span class="icon-circle hide"></span>';// 追加的元素
+
+        $(item).wrap('<div class="radio-parent"></div>').after(spans).addClass('hide');
+        // 只让最后一个单选框被选中
+        if(item.hasAttribute('checked')){
+            items.push(item);
+        }
+    });
+    $(items[items.length - 1]).siblings('.icon-circle').removeClass('hide');//给最后一个包含checked的 input-radio 选中
+
+    $('.radio-parent').click(function (){
+        $(this).siblings().children('.icon-circle').addClass('hide');
+        $(this).children('.icon-circle').toggleClass('hide');
     });
 
-    // 点击icon
-    $(item).children('i').click(function (e){
-        e.stopPropagation();
-        $(item).children('a').trigger('click');// 相当于点击了 a 标签
+
+    /*
+     *   多选框
+     * */
+    $('.check-box-group input:checkbox').each(function (index, item){
+        var spans = '<span class="icon-check-empty"></span><span class="icon-check hide"></span>';// 追加的元素
+
+        $(item).wrap('<div class="check-parent"></div>').after(spans).addClass('hide');
+        if(item.hasAttribute('checked')){
+            $(item).siblings('.icon-check').removeClass('hide');
+        }
+    });
+    $('.check-parent').click(function (){
+        $(this).children('.icon-check').toggleClass('hide');
     });
 
-    // 点击选项 -- 给 a 标签赋值
-    $(item).find('li').click(function (e){
-        e.stopPropagation();
-        var _a = $(this).parent().siblings('a');
-        var txt = $(this).text();
-        var val = $(this).data('value');
-        _a.text(txt);
-        _a.attr('data-value', val);
-        _a.siblings('ul').slideToggle(100);
-        _a.siblings('i').css({'transform':'rotate(0deg)'});
-        $('.dropdown').removeClass('flag-open');
-        $(this).parent().parent().toggleClass('drop-hover-border');
-    });
-});
 
-/*
- *   动态生成li标签，重新给 li 标签加上 data-value 属性
- * */
-function dynamicDataValue (){
+    /*
+     *   下拉框 -- 自定义属性data-value
+     *   1. 下拉框的的宽度保持一致
+     * */
+// 1. 下拉框的的宽度保持一致
+    $('.dropdown').children('ul').each(function (index, item){
+        var width = $(item).parent('.dropdown').css('width');
+        $(item).css('width', width);
+    })
+
     $('.dropdown').each(function (index, item){
+        //追加icon
+        $(item).prepend('<i class="icon-arrow-down"></i>');
+
+        // a 标签设置 自定义属性
+        $(item).children('a').attr('data-value', '');
+
+        // li 标签设置 自定义属性
         $(item).children('ul').each(function (index, item){
             $(item).children().each(function (index, item){
                 $(item).attr('data-value', index);
             })
         });
-    })
+
+        // 点击输入框
+        $(item).children('a').click(function (e){
+            e.stopPropagation();
+            if($(this).parent().hasClass('flag-open')){
+                $(this).siblings('i').css({'transform':'rotate(0deg)'});
+                $(this).siblings('ul').slideUp(100);
+                $(this).parent().removeClass('flag-open').toggleClass('drop-hover-border');
+                return;
+            }
+            $('.dropdown').removeClass('flag-open').removeClass('drop-hover-border');
+            $('.dropdown ul').css({'display':'none'});// 点击页面任何地方，下拉框隐藏
+            $('.dropdown i').css({'transform':'rotate(0deg)'});
+            $(this).parent().addClass('flag-open').toggleClass('drop-hover-border');
+            $(this).siblings('i').css({'transform':'rotate(180deg)'});
+            $(this).siblings('ul').slideToggle(100);
+        });
+
+        // 点击icon
+        $(item).children('i').click(function (e){
+            e.stopPropagation();
+            $(item).children('a').trigger('click');// 相当于点击了 a 标签
+        });
+
+        // 点击选项 -- 给 a 标签赋值
+        $(item).find('li').click(function (e){
+            e.stopPropagation();
+            var _a = $(this).parent().siblings('a');
+            var txt = $(this).text();
+            var val = $(this).data('value');
+            _a.text(txt);
+            _a.attr('data-value', val);
+            _a.siblings('ul').slideToggle(100);
+            _a.siblings('i').css({'transform':'rotate(0deg)'});
+            $('.dropdown').removeClass('flag-open');
+            $(this).parent().parent().toggleClass('drop-hover-border');
+        });
+    });
+
+    /*
+     *   动态生成li标签，重新给 li 标签加上 data-value 属性
+     * */
+    function dynamicDataValue (){
+        $('.dropdown').each(function (index, item){
+            $(item).children('ul').each(function (index, item){
+                $(item).children().each(function (index, item){
+                    $(item).attr('data-value', index);
+                })
+            });
+        })
+    };
 };
-
-
-
-
-
-
-/*
- *   多选框
- * */
-$('.check-box-group input:checkbox').each(function (index, item){
-    var spans = '<span class="icon-check-empty"></span><span class="icon-check hide"></span>';// 追加的元素
-
-    $(item).wrap('<div class="check-parent"></div>').after(spans).addClass('hide');
-    if(item.hasAttribute('checked')){
-        $(item).siblings('.icon-check').removeClass('hide');
-    }
-});
-$('.check-parent').click(function (){
-    $(this).children('.icon-check').toggleClass('hide');
-});
-
-
-
-
-
-/*
- *   单选框
- *   1. 只有最后一个包含 checked 的 input 才能被选中
- *   2. 点击事件
- * */
-var items = []; // 所有包含 checked 属性的 input-radio
-$('.radio-group input:radio').each(function (index, item){
-    var spans = '<span class="icon-circle-empty"></span><span class="icon-circle hide"></span>';// 追加的元素
-
-    $(item).wrap('<div class="radio-parent"></div>').after(spans).addClass('hide');
-    // 只让最后一个单选框被选中
-    if(item.hasAttribute('checked')){
-        items.push(item);
-    }
-});
-$(items[items.length - 1]).siblings('.icon-circle').removeClass('hide');//给最后一个包含checked的 input-radio 选中
-
-$('.radio-parent').click(function (){
-    $(this).siblings().children('.icon-circle').addClass('hide');
-    $(this).children('.icon-circle').toggleClass('hide');
-});
-
-
 
 
 
@@ -183,9 +190,6 @@ function tabEffect (dom, cls){
         })
     })
 };
-
-
-
 
 
 /*
