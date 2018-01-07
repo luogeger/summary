0644a88dd895acfba2d3649f0a2376cba4a5b1fa
luogeger <xiaoqing1232@163.com>
2018-01-07 11:28:59 +0800

    deleted

--- ES6/source-dir/01es6.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by luogege on 2017.10.11.
- */
-
-
-"use strict"
-
-console.log([3,4,5].map( x => x * x));
--- ES6/source-dir/02es6.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by luogege on 2017.10.11.
- */
-
-
-"use strict"
-
-console.log([5,6,7].map( x => x * x));
